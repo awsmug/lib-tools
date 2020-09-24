315dfc75fa61f704e93a8ae28a701d7b913d0e1e
Sven Wagener <sven@awesome.ug>
2020-09-24 09:44:45 +0200

    Added tests

---
+++ tests/CallerDetectiveTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+use AWSM\LibTools\CallerDetective;
+
+final class CallerDetectiveTest extends TestCase {
+	public function testFunction(): void {
+		$this->assertEquals( 'testFunction', CallerDetective::detect()->functionName() );
+        $this->assertFalse( CallerDetective::detect()->functionHasArgs() );
+	}
+
+	public function testClass(): void {
+		$this->assertTrue( CallerDetective::detect()->isClass() );
+		$this->assertEquals( 'CallerDetectiveTest', CallerDetective::detect()->className() );
+	}
+}
